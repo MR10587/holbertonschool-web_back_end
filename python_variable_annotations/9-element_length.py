a92de50e7490b70c83f00d5b5b15f64b4296de4c
MR10587 <ramalmirzezade20@gmail.com>
2026-02-13 13:49:22 +0400

    .

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -5,5 +5,14 @@
 import typing
 
 
-def element_length(lst: typing.Iterable[typing.Sequence]) -> typing.List[typing.Tuple[typing.Sequence, int]]:
+def element_length(lst: typing.Iterable[typing.Sequence]) \
+-> typing.List[typing.Tuple[typing.Sequence, int]]:
+    '''
+    Docstring for element_length
+    
+    :param lst: Description
+    :type lst: typing.Iterable[typing.Sequence]
+    :return: Description
+    :rtype: List[Tuple[Sequence, int]]
+    '''
     return [(i, len(i)) for i in lst]
