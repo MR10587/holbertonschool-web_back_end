90e62508a5725fc1d444234bb85bbd4641cf3373
MR10587 <ramalmirzezade20@gmail.com>
2026-02-13 13:37:05 +0400

    .

---
+++ python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''STRING'''
+
+
+import typing
+
+def sum_mixed_list(mxd_lst: typing.List[typing.Union[int, float]]) -> float:
+    '''sum mixed list'''
+    return sum(mxd_lst)
