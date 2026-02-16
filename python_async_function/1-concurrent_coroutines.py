45413d2d111376cb8f802c6011fc835d83b7c93d
MR10587 <ramalmirzezade20@gmail.com>
2026-02-16 18:09:10 +0400

    .

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''ASYNC'''
+
+
+from 0-basic_async_syntax import wait_random
+import asyncio
+
+
+async def wait_n(n: int, max_delay: int) -> list[float]:
+    '''DEF'''
+    delays = []
+    for i in range(0, n):
+        delays.append(await wait_random(max_delay))
+    return delays
