8c7aa075656cf7aa655e43838b12fe17b9d390bf
MR10587 <ramalmirzezade20@gmail.com>
2026-02-18 17:47:20 +0400

    .

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -2,8 +2,8 @@
 '''Tasks'''
 
 
-wait_random = __import__('0-basic_async_syntax').wait_random
 import asyncio
+wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> asyncio.Task:
