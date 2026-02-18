21ccb7a27737a98e264531775c8420603d2317ff
MR10587 <ramalmirzezade20@gmail.com>
2026-02-18 17:52:33 +0400

    .

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -2,7 +2,7 @@
 '''New function'''
 
 
-task_wait_random = __import__('3-tasks.py').task_wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
     '''DEF wait_n'''
