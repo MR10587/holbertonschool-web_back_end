34005f527fad010282f61332708d05960c275d70
MR10587 <ramalmirzezade20@gmail.com>
2026-02-18 17:51:59 +0400

    .

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+'''New function'''
+
+
+task_wait_random = __import__('3-tasks.py').task_wait_random
+
+async def task_wait_n(n: int, max_delay: int) -> typing.List[float]:
+    '''DEF wait_n'''
+    tasks = [task_wait_random(max_delay) for i in range(n)]
+    delays = await asyncio.gather(*tasks)
+    result = []
+    for delay in delays:
+        inserted = False
+        for j in range(len(result)):
+            if delay < result[j]:
+                result.insert(j, delay)
+                inserted = True
+                break
+        if not inserted:
+            result.append(delay)
+    return result
