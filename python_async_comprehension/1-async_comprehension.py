4c16490cb733f99075c02468956b3a124ac11ced
MR10587 <ramalmirzezade20@gmail.com>
2026-02-19 16:53:00 +0400

    .

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -7,5 +7,6 @@ async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
+    '''Comprehension'''
     result = [i async for i in async_generator()]
     return result
