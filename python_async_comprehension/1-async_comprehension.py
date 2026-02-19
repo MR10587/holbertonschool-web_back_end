24cdb5a38edf5257fee474473c34fabee32c9838
MR10587 <ramalmirzezade20@gmail.com>
2026-02-19 16:52:09 +0400

    .

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -2,11 +2,10 @@
 '''Generators'''
 
 
-import asyncio
 import typing
 async_generator = __import__('0-async_generator').async_generator
 
 
 async def async_comprehension() -> typing.List[float]:
-    result = [i async for i in await async_generator()]
+    result = [i async for i in async_generator()]
     return result
