371f2592c59030721d261d288e4b1fba64337286
MR10587 <ramalmirzezade20@gmail.com>
2026-02-19 17:01:46 +0400

    .

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -10,6 +10,6 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     start = time.perf_counter()
     tasks = [async_comprehension() for i in range(4)]
-    await asyncio.gather(tasks)
+    await asyncio.gather(*tasks)
     elapsed = time.perf_counter() - start
     return elapsed
