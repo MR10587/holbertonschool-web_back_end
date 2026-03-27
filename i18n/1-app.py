dc67503751ccb4154c4790beca6f3cfd7e7e7201
MR10587 <ramalmirzezade20@gmail.com>
2026-03-27 09:23:45 +0000

    .

---
+++ i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''Basic Flask App'''
+from flask import Flask, render_template
+from flask-babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    app.config["BABEL_DEFAULT_LOCALE"] = "en"
+    app.config["BABEL_DEFAULT_TIMEZONE"] = "UTC"
+
+
+@app.route("/")
+def index():
+    '''INDEX PAGE'''
+    return render_template('0-index.html')
+
+
+if __name__ == "__main__":
+    app.run()
