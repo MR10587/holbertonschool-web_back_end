72a6db86e53f3c4936dce029f686685355017883
MR10587 <ramalmirzezade20@gmail.com>
2026-03-27 08:37:29 +0000

    .

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+'''Basic Flask App'''
 from flask import flask, render_template
+
 
 
 app = Flask()
 
 @app.route("/")
 def index():
+    '''INDEX PAGE'''
     return render_template("0-index.html")
