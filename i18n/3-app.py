aca3fdfe0ee46dd6bb074fc9a2c61b6cb8c939ff
MR10587 <ramalmirzezade20@gmail.com>
2026-03-31 16:55:07 +0400

    .# Please enter the commit message for your changes. Lines starting

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -1,39 +1,34 @@
 #!/usr/bin/env python3
-'''Basic Flask App'''
+"""3-app.py"""
 from flask import Flask, render_template, request
 from flask_babel import Babel
 
 
+class Config(object):
+    """Config class for Babel"""
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+    BABEL_TRANSLATION_DIRECTORIES = 'translations'
+
+
 app = Flask(__name__)
-
-
-class Config:
-    '''Configuration'''
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
-
 app.config.from_object(Config)
-
-babel = Babel(app)
 
 
 def get_locale():
-    '''GET LOCAL'''
-    return request.accept_languages.best_match(
-        app.config['LANGUAGES']
-    ) or app.config['BABEL_DEFAULT_LOCALE']
+    """Determines supported lang."""
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+babel = Babel(app, locale_selector=get_locale)
 
 
-@app.route("/")
-def index():
-    '''INDEX PAGE''''
-    return render_template('3-index.html')
+@app.route('/')
+def main_page():
+    """Main page route that renders the index template"""
+    return render_template('3-index.html'), 200
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run()
