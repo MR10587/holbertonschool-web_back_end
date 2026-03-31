47f0320700e06ddb0b801caa9cba782c81f1d686
MR10587 <ramalmirzezade20@gmail.com>
2026-03-31 16:52:18 +0400

    .

--- i18n/3-app.py
+++ i18n/3-app.py
@@ -32,7 +32,7 @@ babel.init_app(app, locale_selector=get_locale)
 @app.route("/")
 def index():
     '''INDEX PAGE''''
-    return render_template('2-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
