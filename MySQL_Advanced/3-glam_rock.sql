45c8d4908ac2647e3f32eb8f4ae1483512fef3b8
MR10587 <ramalmirzezade20@gmail.com>
2026-05-03 21:57:32 +0400

    SQL

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,7 +1,7 @@
 -- SQL
-SELECT 
+SELECT
     band_name,
-    (2024 - formed) AS lifespan
-FROM bands
-WHERE style = 'Glam rock'
+    (IFNULL(split, 2024) - formed) AS lifespan
+FROM metal_bands
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
