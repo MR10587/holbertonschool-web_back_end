8e04a93237519f1fd9b1c9e26ab49dfefa79e625
MR10587 <ramalmirzezade20@gmail.com>
2026-05-05 06:40:10 +0000

    .

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -15,6 +15,7 @@ def count_calls(method: Callable) -> Callable:
         return method(self, *args, **kwargs)
     return wrapper
 
+
 def call_history(method: Callable) -> Callable:
     '''Input and output history'''
     @wraps(method)
@@ -28,13 +29,14 @@ def call_history(method: Callable) -> Callable:
         return output
     return wrapper
 
+
 class Cache:
     '''Cache'''
     def __init__(self):
         '''Init'''
         self._redis = redis.Redis()
         self._redis.flushdb()
-    
+
     @count_calls
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
@@ -46,21 +48,20 @@ class Cache:
     def get(self, key: str, fn: Optional[Callable] = None):
         '''Get type'''
         value = self._redis.get(key)
-
         if value is None:
            return value
-
         if fn:
             return fn(value)
         return value
 
     def get_str(self, key):
         '''String'''
-        return self.get(key, fn = lambda d: d.decode("utf-8"))
+        return self.get(key, fn=lambda d: d.decode("utf-8"))
 
     def get_int(self, key):
         '''Integer'''
         return self.get(key, int)
+
 
 def replay(method: Callable):
     '''Function to show input and outputs'''
