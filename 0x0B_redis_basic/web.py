2a42463a4bbe0f8a6b52add0fe88bb2c34478bb3
MR10587 <ramalmirzezade20@gmail.com>
2026-05-05 07:14:21 +0000

    .

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -10,9 +10,9 @@ r = Redis()
 def get_page(url: str) -> str:
     '''Get Page and return its content while caching its value for 10 seconds'''
     r.incr(f"count:{url}")
-    cache = r.get(url)
+    cache = r.get(f"count:{url}") 
     if cache:
         return cache.decode("utf-8")
     resp = requests.get(url)
-    r.setex(url, 10, resp.text)
+    r.setex(f"count:{url}", 10, resp.text)
     return resp.text
