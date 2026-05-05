2640fabf851ab646ce1e2249c9d19549763a7916
MR10587 <ramalmirzezade20@gmail.com>
2026-05-05 07:05:30 +0000

    implement function inside web.py to store count of access to website using request module and return response

---
+++ 0x0B_redis_basic/web.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''Web'''
+import requests
+from redis import Redis
+
+
+r = Redis()
+count = 0
+
+
+def get_page(url: str) -> str:
+    count += 1
+    r.set("count:{url}", count, ex=10)
+    resp = requests.get(url)
+    return resp
+
+print(get_page("https://httpbin.org/delay/3"))
