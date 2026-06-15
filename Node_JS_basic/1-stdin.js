43d2bb16c4ce4bd915cdec7fd10704009d39260b
MR10587 <ramalmirzezade20@gmail.com>
2026-06-15 11:02:07 +0400

    .

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,15 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-process.stdin.setEncoding("utf-8");
+process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (data) => {
-    process.stdout.write('This important software is now closing\n');
+process.stdin.on('readable', () => {
+  const data = process.stdin.read();
+
+  if (data !== null) {
+    process.stdout.write(`Your name is: ${data}`);
+  }
+});
+
+process.stdin.on('end', () => {
+  process.stdout.write('This important software is now closing\n');
 });
