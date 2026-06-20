9458b3c3c2b7013225ace1a07d4673f02f6b5a9e
MR10587 <ramalmirzezade20@gmail.com>
2026-06-20 15:45:22 +0400

    .

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -9,3 +9,5 @@ const server = http.createServer((req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is successfully listening on http://localhost:${PORT}`);
 });
+
+module.exports = app;
