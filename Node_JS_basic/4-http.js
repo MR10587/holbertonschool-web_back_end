ec4a2d7633780bb637caed3d1c5dc68165ea9397
MR10587 <ramalmirzezade20@gmail.com>
2026-06-20 15:42:48 +0400

    .

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const app = require('http');
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, {'Content-type': 'text/plain'});
+
+    res.end('Hello Holberton School!');
+});
+
+server.listen(PORT, () => {
+  console.log(`Server is successfully listening on http://localhost:${PORT}`);
+});
