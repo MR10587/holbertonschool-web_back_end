5f8dfe343ecac912303dec05c1126c7c1d8f4c00
MR10587 <ramalmirzezade20@gmail.com>
2026-06-20 15:51:26 +0400

    .

--- Node_JS_basic/4-http.js
+++ Node_JS_basic/4-http.js
@@ -1,10 +1,10 @@
 const http = require('http');
 
-const app = http.createServer((req, res) => {
-    res.writeHead(200, {'Content-type': 'text/plain'});
+const app = http.createServer((req, res) => { 
+  res.writeHead(200, {'Content-type': 'text/plain'});
 
-    res.end('Hello Holberton School!');
-});
+  res.end('Hello Holberton School!');
+ });
 
 app.listen(1245);
 
