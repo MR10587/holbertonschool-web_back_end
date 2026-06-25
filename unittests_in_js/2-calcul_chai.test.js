e2053c19162f5dbca3b30d3043d79e63edfc2496
MR10587 <ramalmirzezade20@gmail.com>
2026-06-25 14:03:51 +0400

    .

---
+++ unittests_in_js/2-calcul_chai.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai');
+const calculateNumber = require('./1-calcul');
+
+describe('calculateNumber', () => {
+  it("should return 6 when adding 1.4 and 4.5", () => {
+    expect(calculateNumber('SUM', 1.4, 4.5)).to.equal(6);
+  });
+
+  it("should return -4 when subtracting 1.4 and 4.5", () => {
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5).to.equal(-4));
+  });
+
+  it("should return 0.2 when dividing 1.4 and 4.5", () => {
+    expect(calculateNumber('DIVIDE', 1.4, 1.5).to.equal(0.2));
+  });
+
+  it("should return 'Error' when dividing 1.4 and 0", () => {
+    expect(calculateNumber('DIVIDE', 1.4, 0).to.equal('Error'));
+  });
+});
