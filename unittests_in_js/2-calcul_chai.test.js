95b5fcd3220459053839804f4238efc3b06c73a4
MR10587 <ramalmirzezade20@gmail.com>
2026-06-25 14:06:05 +0400

    .

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -7,14 +7,14 @@ describe('calculateNumber', () => {
   });
 
   it("should return -4 when subtracting 1.4 and 4.5", () => {
-    expect(calculateNumber('SUBTRACT', 1.4, 4.5).to.equal(-4));
+    expect(calculateNumber('SUBTRACT', 1.4, 4.5)).to.equal(-4);
   });
 
   it("should return 0.2 when dividing 1.4 and 4.5", () => {
-    expect(calculateNumber('DIVIDE', 1.4, 1.5).to.equal(0.2));
+    expect(calculateNumber('DIVIDE', 1.4, 1.5)).to.equal(0.2);
   });
 
   it("should return 'Error' when dividing 1.4 and 0", () => {
-    expect(calculateNumber('DIVIDE', 1.4, 0).to.equal('Error'));
+    expect(calculateNumber('DIVIDE', 1.4, 0)).to.equal('Error');
   });
 });
