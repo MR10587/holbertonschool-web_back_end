85c5875cb169226759e7004d91eeae92e6336946
MR10587 <ramalmirzezade20@gmail.com>
2026-06-28 11:53:42 +0400

    .

--- unittests_in_js/4-payment.test.js
+++ unittests_in_js/4-payment.test.js
@@ -4,24 +4,22 @@ const { expect } = require("chai");
 const Utils = require("./utils");
 const sendPaymentRequestToApi = require("./3-payment");
 
-describe('sendPaymentRequestToApi', () => {
+describe("sendPaymentRequestToApi", () => {
   afterEach(() => {
     sinon.restore();
   });
 
-  it('should log the correct message', () => {
-    const calculateStub = sinon.stub(
-        Utils, 'calculateNumber'
-    ).returns(10);
+  it("should log the correct message", () => {
+    const calculateStub = sinon.stub(Utils, "calculateNumber").returns(10);
 
-    const logSpy = sinon.spy(console, 'log');
+    const logSpy = sinon.spy(console, "log");
 
     sendPaymentRequestToApi(100, 20);
 
     expect(calculateStub.calledOnce).to.equal(true);
-    expect(calculateStub.calledWith('SUM', 100, 20)).to.equal(true);
+    expect(calculateStub.calledWith("SUM", 100, 20)).to.equal(true);
 
     expect(logSpy.calledOnce).to.equal(true);
-    expect(logSpy.calledWith('The total is: 10')).to.equal(true);
+    expect(logSpy.calledWith("The total is: 10")).to.equal(true);
   });
 });
