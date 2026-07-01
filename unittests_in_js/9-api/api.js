ca8420b3712f1f5da52666b9ec333c18dd594cde
MR10587 <ramalmirzezade20@gmail.com>
2026-07-01 13:22:07 +0400

    .

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -13,8 +13,10 @@ app.get("/card/:id(\\d+)", (req, res) => {
   res.send(`Payment methods for card ${id}`);
 });
 
-app.listen(port, () => {
-  console.log(`API available on localhost port ${port}`);
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+  });
+}
 
 module.exports = app;
