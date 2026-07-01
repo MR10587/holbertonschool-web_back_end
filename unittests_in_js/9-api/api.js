fb6cc4cf5164fa71781d01b89743d8f11cb8e162
MR10587 <ramalmirzezade20@gmail.com>
2026-07-01 13:04:03 +0400

    .

---
+++ unittests_in_js/9-api/api.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const port = 7865;
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the payment system");
+});
+
+app.get("/card/:id", (req, res) => {
+  let id = req.params["id"];
+
+  if(Number.isInteger(id)) {
+    res.send(`Payment methods for card ${id}`);
+  } else {
+    res.sendStatus(404);
+  }
+})
+
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(`API available on localhost port ${port}`);
+  });
+}
+
+module.exports = app;
